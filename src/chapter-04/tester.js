00ea172803eb5aa44c646f426b4f0067acb94c39
jungon <j017.kim@gmail.com>
2021-08-17 23:12:20 +0900

    chapter04 - first test

---
+++ src/chapter-04/tester.js
@@ -0,0 +1,110 @@
+const sampleProvinceData = {
+  name: "Asia",
+  producers: [
+    { name: "김중온", cost: 10, production: 9 },
+    { name: "김지헌", cost: 12, production: 10 },
+    { name: "이은숙", cost: 10, production: 6 },
+  ],
+  demand: 30,
+  price: 20,
+};
+
+class Province {
+  constructor(doc) {
+    this._name = doc.name;
+    this._producers = [];
+    this._totalProduction = 0;
+    this._demand = doc.demand;
+    this._price = doc.price;
+    if (Array.isArray(doc.producers)) {
+      doc.producers.forEach((it) => {
+        this.addProducer(new Producer(this, it));
+      });
+    }
+  }
+
+  addProducer(arg) {
+    this._producers.push(arg);
+    this._totalProduction += arg.production;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get producers() {
+    return this._producers.slice();
+  }
+  get totalProduction() {
+    return this._totalProduction;
+  }
+  set totalProduction(arg) {
+    this._totalProduction = arg;
+  }
+  get demand() {
+    return this._demand;
+  }
+  set demand(arg) {
+    this._demand = parseInt(arg);
+  }
+  get price() {
+    return this._price;
+  }
+  set price(arg) {
+    this._price = parseInt(arg);
+  }
+  get shortfall() {
+    return this._demand - this.totalProduction;
+  }
+  get profit() {
+    return this.demandValue - this.demandCost;
+  }
+  get demandValue() {
+    return this.satisfiedDemand * this.price;
+  }
+  get satisfiedDemand() {
+    return Math.min(this._demand, this.totalProduction);
+  }
+  get demandCost() {
+    let remainingDemand = this.demand;
+    let result = 0;
+    this.producers
+      .sort((a, b) => a.cost - b.cost)
+      .forEach((it) => {
+        const contribution = Math.min(remainingDemand, it.production);
+        remainingDemand -= contribution;
+        result += contribution * it.cost;
+      });
+    return result;
+  }
+}
+
+class Producer {
+  constructor(aProvince, data) {
+    this._province = aProvince;
+    this._cost = data.cost;
+    this._name = data.name;
+    this._production = data.production ?? 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get cost() {
+    return this._cost;
+  }
+  set cost(arg) {
+    this._cost = parseInt(arg);
+  }
+
+  get production() {
+    return this._production;
+  }
+  set production(amountStr) {
+    const amount = parseInt(amountStr);
+    const newProduction = Number.isNaN(amount) ? 0 : amount;
+    this._province.totalProduction += newProduction - this._production;
+    this._production = newProduction;
+  }
+}
+
+export { sampleProvinceData, Province, Producer };
